d25f22784b0c88a773fdccb54d4ea2b2b875d5c0
Lethabo Faith Kekana <kekanalethabo75@gmail.com>
2024-09-29 15:43:45 +0200

    added github repo url

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
        
         <p>
           Weather App
         </p>
         
-      </header>
+        <footer>
+          This website was coded by <a href='https://github.com/Faithk33'>Lethabo Kekana</a> and is open-sourced on <a href='https://github.com/Faithk33/react-weather-project'>Github</a> and hosted by
+         <a href='https://react-weather-lkekana.netlify.app/'>Netlify</a>
+        </footer>
+      
     </div>
   );
 }
