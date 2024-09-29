4ea562fa1f8e2c017c97b50d969d0b18508b7470
Lethabo Faith Kekana <kekanalethabo75@gmail.com>
2024-09-29 15:31:05 +0200

    removed unnecessary code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Weather App
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+
