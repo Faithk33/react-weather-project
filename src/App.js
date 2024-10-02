75ba74dadf95e78d54c780a37b6d0734fbd233c3
Lethabo Faith Kekana <kekanalethabo75@gmail.com>
2024-10-03 01:08:13 +0200

    improved basic setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-       
-        <p>
+       <div className="container">
+        <h1>
           Weather App
-        </p>
-        
+        </h1>
+        <Weather/>
         <footer>
-          This website was coded by <a href='https://github.com/Faithk33'>Lethabo Kekana</a> and is open-sourced on <a href='https://github.com/Faithk33/react-weather-project'>Github</a> and hosted by
+          This website was coded by 
+          <a href='https://github.com/Faithk33'>Lethabo Kekana</a>
+           and is open-sourced on 
+           <a href='https://github.com/Faithk33/react-weather-project'>Github</a> 
+           and hosted by
          <a href='https://react-weather-lkekana.netlify.app/'>Netlify</a>
         </footer>
-      
+      </div>
     </div>
   );
 }
