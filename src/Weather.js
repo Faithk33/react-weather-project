47b4b97f8f31324f4a39108a1e89c5c23c82e832
Lethabo Faith Kekana <kekanalethabo75@gmail.com>
2024-10-05 00:33:28 +0200

    added styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,34 +4,41 @@ import "./Weather.css";
 export default function Weather(){
     return(
         <div className="Weather">
+            <form>
             <div className="row">
-                <div className="col-3">
-                    <form>
-                <input type="search" placeholder="Enter a city" className="form-control"/>
-                </form>
+                <div className="col-9">
+                     
+                <input type="search" placeholder="Enter a city" className="form-control search-input"/>
+                
             </div>
-            <div className="col-9">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+            <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                
             </div>
             </div>
+            </form>
+            <div className="WeatherInfo">
             <h1>New York</h1>
             <ul>
-                <li>Wednesday, 19:00</li>
+                <li>Wednesday, <span>19:00</span></li>
                 <li>Cloudy</li>
             </ul>
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy"/>
-                    18°C
             
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-8">
+                    <div className="temp-container d-flex justify-content-end">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy"/>
+                    <span className="current-temperature">18</span><span className="unit">°C</span>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Precipitation: 1%</li>
-                        <li>Humidity: 69%</li>
-                        <li>Wind: 10 km/h</li>
+                        <li>Precipitation: <strong>1%</strong></li>
+                        <li>Humidity: <strong>69%</strong></li>
+                        <li>Wind: <strong>10 km/h</strong></li>
                     </ul>
                 </div>
+            </div>
             </div>
         </div>
     )
